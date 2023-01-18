87fa02c42ac2c2c3713a86c2edd759cf0b461938
jude <judenam4655@gmail.com>
2023-01-18 09:36:05 +0900

    edited import

--- board/BoardDAO.java
+++ board/BoardDAO.java
@@ -9,7 +9,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import BoardDAO;
+import board.BoardDTO;
 
 public class BoardDAO {
 	private DataSource ds;
